db617a263c2577741acd1fe2dd523d26ca1a2eca
SetiZ <SetiZ@users.noreply.github.com>
2020-11-18 22:10:52 +0100

    fix text

--- src/generate-pdf.js
+++ src/generate-pdf.js
@@ -4,7 +4,7 @@ const puppeteer = require('puppeteer');
   const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
   await page.goto('https://setiz.github.io/resume/', {waitUntil: 'networkidle0'});
-  const pdf = await page.pdf({path: '.resume.pdf', format: 'A4' });
+  const pdf = await page.pdf({path: 'resume.pdf', format: 'A4', margin: { left: '2cm', top: '4cm', right: '1cm', bottom: '3.5cm' } });
 
   await browser.close();
 })();
